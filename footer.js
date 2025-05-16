a732e229b15e3d39d4a7a1974014a6541e8bbddb
RichardODBM <richard.mee@ODB.org>
2025-05-16 16:40:23 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,1 +1,9 @@
-
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    fetch('footer.html')
+      .then(res => res.text())
+      .then(html => {
+        document.getElementById('footer').innerHTML = html;
+      });
+  });
+</script>
